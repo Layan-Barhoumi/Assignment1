08623281c37b1145f039f10f9e0ad0301aa5fb6b
Layan-Barhoumi <79206902+Layan-Barhoumi@users.noreply.github.com>
2021-02-20 16:26:51 +0200

    Update server.py

--- server.py
+++ server.py
@@ -3,15 +3,13 @@ from datetime import datetime
 
 IP = "127.0.0.1"
 port = 12345
-BUFFER_SIZE = 4096
+BUFFER_SIZE = 54
 
 #create socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 #bind
 s.bind((IP,port))
-
-data=""
 
 #create infinite loop
 while (True):
@@ -20,6 +18,7 @@ while (True):
     data,addr=s.recvfrom(BUFFER_SIZE)
     
     #date-time syntax taken from example 3 (https://www.programiz.com/python-programming/datetime/current-datetime)
-    if (data!=""):
-        print ("Request received on ",datetime.now().strftime("%d/%m/%Y")," at ",datetime.now().strftime("%H:%M:%S"))
-        s.sendto(data,addr)
+    print ("Request received on ",datetime.now().strftime("%d/%m/%Y")," at ",datetime.now().strftime("%H:%M:%S"))
+    s.sendto(data,addr)
+    
+    
